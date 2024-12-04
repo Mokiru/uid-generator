15fa4572e3f0f47badddc98b79ae1360da5a8f55
栞栞谁没有六星 <12408285+koroz@user.noreply.gitee.com>
2024-12-04 16:55:10 +0800

    修改使用Cached加快id生成

--- testuid/src/main/java/cn/momochi/testuid/spring/BaseConfiguration.java
+++ testuid/src/main/java/cn/momochi/testuid/spring/BaseConfiguration.java
@@ -1,6 +1,7 @@
 package cn.momochi.testuid.spring;
 
 import cn.momochi.uid.UidGenerator;
+import cn.momochi.uid.impl.CachedUidGenerator;
 import cn.momochi.uid.impl.DefaultUidGenerator;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,14 +14,27 @@ public class BaseConfiguration {
     @Autowired
     private DisposableWorkerIdAssigner disposableWorkerIdAssigner;
 
+//    @Bean
+//    public UidGenerator uidGenerator() {
+//        DefaultUidGenerator instance = new DefaultUidGenerator();
+//        instance.setWorkerIdAssigner(disposableWorkerIdAssigner);
+//        instance.setTimeBits(29);
+//        instance.setWorkerBits(21);
+//        instance.setSeqBits(13);
+//        instance.setEpochStr("2016-09-20");
+//        return instance;
+//    }
+
     @Bean
     public UidGenerator uidGenerator() {
-        DefaultUidGenerator instance = new DefaultUidGenerator();
-        instance.setWorkerIdAssigner(disposableWorkerIdAssigner);
+        CachedUidGenerator instance = new CachedUidGenerator();
         instance.setTimeBits(29);
         instance.setWorkerBits(21);
         instance.setSeqBits(13);
         instance.setEpochStr("2016-09-20");
+        instance.setBoostPower(3);
+        instance.setScheduleInterval(60);
+        instance.setWorkerIdAssigner(disposableWorkerIdAssigner);
         return instance;
     }
 }
